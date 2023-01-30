62889d3744ec00fe8a09fdc000a7fbc1e8268956
FTigashov <tigashov.filipp@yandex.ru>
2023-01-30 20:43:12 +0300

    updated chatcontroller file

--- src/main/java/com/personal/networkchat/client/controllers/ChatController.java
+++ src/main/java/com/personal/networkchat/client/controllers/ChatController.java
@@ -1,10 +1,15 @@
 package com.personal.networkchat.client.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
-public class ChatController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ChatController implements Initializable {
     @FXML
     private TextArea chatHistory;
 
@@ -28,4 +33,27 @@ public class ChatController {
 
     @FXML
     private Text userName;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Font mainFont = new Font("Arial", 16);
+        chatHistory.setFont(mainFont);
+        inputField.setFont(mainFont);
+        sendButton.setOnAction(e -> sendMessage());
+    }
+
+    private void sendMessage() {
+        String message = inputField.getText().trim();
+        if (message.isBlank()) {
+            inputField.clear();
+            return;
+        }
+        inputField.clear();
+        addMessage(message);
+    }
+
+    private void addMessage(String message) {
+        chatHistory.appendText(message);
+        chatHistory.appendText(System.lineSeparator());
+    }
 }
