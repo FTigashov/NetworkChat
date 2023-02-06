fdda36ed26f1781edb932aa4f5b8c66a12f40373
FTigashov <tigashov.filipp@yandex.ru>
2023-02-06 16:11:57 +0300

    added check empty field in reg frame

--- src/main/java/com/personal/networkchat/client/controllers/RegistrationController.java
+++ src/main/java/com/personal/networkchat/client/controllers/RegistrationController.java
@@ -4,6 +4,7 @@ import com.personal.networkchat.client.ClientApp;
 import com.personal.networkchat.client.models.Network;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -30,6 +31,10 @@ public class RegistrationController {
     private Network network;
     private ClientApp clientApp;
 
+    private final String EMPTY_FIELDS_ERROR = "emptyFields";
+    private final String ACCOUNT_ERROR = "authError";
+    private final String USER_IS_BUSY = "user_is_busy";
+
     public void setNetwork(Network network) {
         this.network = network;
     }
@@ -53,6 +58,40 @@ public class RegistrationController {
 
     @FXML
     void startRegistration(ActionEvent event) {
+        String name, surname, login, password;
+        name = nameField.getText().trim();
+        surname = surnameField.getText().trim();
+        login = loginField.getText().trim();
+        password = pwdField.getText().trim();
+        if (name.isBlank() ||
+        surname.isBlank() ||
+        login.isBlank() ||
+        password.isBlank()) {
+            showError(EMPTY_FIELDS_ERROR);
+            return;
+        }
 
+        String regErrorMessage = null;
+    }
+
+    @FXML
+    void showError(String errorType) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Registration error");
+        switch (errorType) {
+            case "emptyFields":
+                alert.setHeaderText("Some of these fields are empty");
+                alert.setContentText("Make sure that all fields must be filled in.");
+                break;
+            case "authError":
+                alert.setHeaderText("Incorrect login or password");
+                alert.setContentText("Make sure that your login and password are correct.");
+                break;
+            case "user_is_busy":
+                alert.setHeaderText("This user has already logged in to the chat");
+                alert.setContentText("To continue,\nyou need to log out of the chat,\nor log in to another account.");
+                break;
+        }
+        alert.show();
     }
 }
