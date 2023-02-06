27b06a14b546cbee75f62baee9e4d4f573e9c73d
FTigashov <tigashov.filipp@yandex.ru>
2023-02-06 15:49:22 +0300

    added register controller structure and menu bar in chat frame

--- src/main/java/com/personal/networkchat/client/controllers/RegistrationController.java
+++ src/main/java/com/personal/networkchat/client/controllers/RegistrationController.java
@@ -1,4 +1,27 @@
 package com.personal.networkchat.client.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class RegistrationController {
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private TextField loginField;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private PasswordField pwdField;
+
+    @FXML
+    private Button registerButton;
+
+    @FXML
+    private TextField surnameField;
+
 }
