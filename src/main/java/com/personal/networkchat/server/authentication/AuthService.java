0b88b4431b94dc884c1a406bb2edf8515cd85395
FTigashov <tigashov.filipp@yandex.ru>
2023-02-20 12:46:04 +0300

    created diagram demonstrating mvc pattern

--- src/main/java/com/personal/networkchat/server/authentication/AuthService.java
+++ src/main/java/com/personal/networkchat/server/authentication/AuthService.java
@@ -5,8 +5,8 @@ import com.personal.networkchat.server.models.User;
 public interface AuthService {
     User getUserNameByLoginAndPassword(String login, String password);
     User insertNewUser(User user);
-    void startAuthentication();
-    void endAuthentication();
+    void openConnection();
+    void closeConnection();
     String update(User user);
     String delete(User user);
 }
