52cf5af5e0195f8dfdb5b9feb5b3e6aadf98f15d
FTigashov <tigashov.filipp@yandex.ru>
2023-03-18 12:20:23 +0300

    added loggers into project

--- src/main/java/com/personal/networkchat/server/handler/LoggingConfig.java
+++ src/main/java/com/personal/networkchat/server/handler/LoggingConfig.java
@@ -4,13 +4,13 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
 public class LoggingConfig {
-//    protected Logger admin;
-//    protected Logger admin_console;
-//    protected Logger file;
-//
-//    {
-//        PropertyConfigurator.configure("src/main/resources/config/log4j.properties");
-//        admin = Logger.getLogger("admin");
-//        admin_console = Logger.getLogger("admin_console");
-//    }
+    protected Logger admin;
+    protected Logger admin_console;
+    protected Logger file;
+
+    {
+        PropertyConfigurator.configure("src/main/resources/config/log4j.properties");
+        admin = Logger.getLogger("admin");
+        admin_console = Logger.getLogger("admin_console");
+    }
 }
