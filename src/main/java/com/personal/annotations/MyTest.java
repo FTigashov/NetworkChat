35ca58047d481addb3bf18ca6f46faf12de2e0fb
FTigashov <tigashov.filipp@yandex.ru>
2023-03-28 13:30:03 +0300

    created annotations and test class

---
+++ src/main/java/com/personal/annotations/MyTest.java
@@ -0,0 +1,39 @@
+package com.personal.annotations;
+
+public class MyTest {
+
+    @BeforeSuite
+    void init() {
+        System.out.println("Start test...");
+    }
+
+    @Test
+    void testOne() {
+        System.out.println("test 1");
+    }
+
+    @Test(priority = 4)
+    void testTwo() {
+        System.out.println("test 2");
+    }
+
+    @Test(priority = 3)
+    void testThree() {
+        System.out.println("test 3");
+    }
+
+    @Test(priority = 2)
+    void testFour() {
+        System.out.println("test 4");
+    }
+
+    @Test(priority = 1)
+    void testFive() {
+        System.out.println("test 5");
+    }
+
+    @AfterSuite
+    void destroy() {
+        System.out.println("Finish test...");
+    }
+}
---
+++ src/main/java/com/personal/annotations/StartApp.java
@@ -0,0 +1,8 @@
+package com.personal.annotations;
+
+public class StartApp {
+    public static void main(String[] args) {
+        Class c = MyTest.class;
+
+    }
+}
